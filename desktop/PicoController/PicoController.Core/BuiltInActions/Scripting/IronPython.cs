71a712f9f359928f892b4edff98b59e6662f2832
Epat <i.am@epat.xyz>
2023-01-11 18:14:05 +0100

    Adding logger and IDisplayInfo to ironpython scope

--- desktop/PicoController/PicoController.Core/BuiltInActions/Scripting/IronPython.cs
+++ desktop/PicoController/PicoController.Core/BuiltInActions/Scripting/IronPython.cs
@@ -1,30 +1,36 @@
 ﻿using Microsoft.Scripting.Hosting;
 using PicoController.Core;
+using PicoController.Plugin;
 using PicoController.Plugin.Attributes;
+using Serilog;
 using IP = IronPython;
 
 namespace PicoController.Core.BuiltInActions.Scripting;
 
 public class IronPython : IronPythonBase
 {
-    public IronPython() : base(false) { }
+    public IronPython(ILogger logger, IDisplayInfo displayInfo) : base(false, logger, displayInfo) { }
 }
 
 public class IronPythonFile : IronPythonBase
 {
-    public IronPythonFile() : base(true) { }
+    public IronPythonFile(ILogger logger, IDisplayInfo displayInfo) : base(true, logger, displayInfo) { }
 }
 
 [HideHandler]
 public abstract class IronPythonBase : IPluginAction
 {
     private readonly bool _useFile;
+    private readonly ILogger _logger;
+    private readonly IDisplayInfo _displayInfo;
     private ScriptEngine _engine;
 
-    protected IronPythonBase(bool useFile)
+    protected IronPythonBase(bool useFile, ILogger logger, IDisplayInfo displayInfo)
     {
         _engine = IP.Hosting.Python.CreateEngine();
         _useFile = useFile;
+        _logger = logger;
+        _displayInfo = displayInfo;
     }
 
     public async Task ExecuteAsync(int inputValue, string? argument)
@@ -35,9 +41,11 @@ public abstract class IronPythonBase : IPluginAction
             throw new ArgumentNullException("data");
 
         var scope = _engine.CreateScope();
+        
 
         scope.SetVariable("__input_value__", inputValue);
-        //scope.SetVariable("__user_argument__", argument);
+        scope.SetVariable("__logger__", _logger);
+        scope.SetVariable("__display_info__", _displayInfo);
 
         if (_useFile)
             _engine.ExecuteFile(argument, scope);
