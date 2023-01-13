88b1b471d9e848b5daf07d39c86b8b4488a9be55
Epat <i.am@epat.xyz>
2023-01-13 09:21:44 +0100

    lola

--- desktop/PicoController/PicoController.Core/BuiltInActions/Scripting/IronPython.cs
+++ desktop/PicoController/PicoController.Core/BuiltInActions/Scripting/IronPython.cs
@@ -23,7 +23,7 @@ public abstract class IronPythonBase : IPluginAction
     private readonly bool _useFile;
     private readonly ILogger _logger;
     private readonly IDisplayInfo _displayInfo;
-    private ScriptEngine _engine;
+    private readonly ScriptEngine _engine;
 
     protected IronPythonBase(bool useFile, ILogger logger, IDisplayInfo displayInfo)
     {
@@ -35,21 +35,22 @@ public abstract class IronPythonBase : IPluginAction
 
     public async Task ExecuteAsync(int inputValue, string? argument)
     {
-        await Task.Yield();
+        await Task.Run(() =>
+        {
+            if (argument is null)
+                throw new ArgumentNullException("data");
 
-        if (argument is null)
-            throw new ArgumentNullException("data");
+            var scope = _engine.CreateScope();
 
-        var scope = _engine.CreateScope();
-        
 
-        scope.SetVariable("__input_value__", inputValue);
-        scope.SetVariable("__logger__", _logger);
-        scope.SetVariable("__display_info__", _displayInfo);
+            scope.SetVariable("__input_value__", inputValue);
+            scope.SetVariable("__logger__", _logger);
+            scope.SetVariable("__display_info__", _displayInfo);
 
-        if (_useFile)
-            _engine.ExecuteFile(argument, scope);
-        else
-            _engine.Execute(argument, scope);
+            if (_useFile)
+                _engine.ExecuteFile(argument, scope);
+            else
+                _engine.Execute(argument, scope);
+        });
     }
 }
