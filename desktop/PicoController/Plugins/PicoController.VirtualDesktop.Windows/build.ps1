aa5ec23e6d70c00ecce69ec3ec00f8f7400548c2
Epat <i.am@epat.xyz>
2023-01-15 18:53:34 +0100

    fixing broken output path while building Virtual Desktop plugin

--- desktop/PicoController/Plugins/PicoController.VirtualDesktop.Windows/build.ps1
+++ desktop/PicoController/Plugins/PicoController.VirtualDesktop.Windows/build.ps1
@@ -3,8 +3,6 @@ param (
     [string]$Out
 )
 
-$Out = $Out + "PicoController.VirtualDesktop.Windows"
-
 dotnet publish "$PSScriptRoot\PicoController.VirtualDesktop.Windows.csproj" --use-current-runtime --sc --os win -o "$Out";
 
 robocopy "$PSScriptRoot\thirdparty\" "$Out"
