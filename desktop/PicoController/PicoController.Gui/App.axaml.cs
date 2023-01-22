3414910eae4dc80a70f6d9b8ea4d392228ac7568
Epat <i.am@epat.xyz>
2023-01-22 03:47:54 +0100

    update to system tray menu

--- desktop/PicoController/PicoController.Gui/App.axaml.cs
+++ desktop/PicoController/PicoController.Gui/App.axaml.cs
@@ -18,39 +18,83 @@ using Serilog.Formatting.Compact;
 using Serilog.Sinks.Discord;
 using Splat;
 using System;
+using System.ComponentModel;
 using System.Reactive.Linq;
 
 namespace PicoController.Gui;
 
 public class App : Application
 {
+    private TrayIcon _trayIcon;
+
+    public App()
+    {
+        DataContext = this;
+        _clickResetTimer = new()
+        {
+            Interval = 400,
+            AutoReset = false,
+        };
+        _clickResetTimer.Elapsed += _clickResetTimer_Elapsed;
+    }
+
+    private void _clickResetTimer_Elapsed(object? sender, System.Timers.ElapsedEventArgs e)
+    {
+        _clickResetTimer.Stop();
+        _openWindowAfterNextClick = false;
+    }
+
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
     }
-
+    public IMainWindowViewModel? MainWindowViewModel { get; set; }
     public static IClassicDesktopStyleApplicationLifetime? DesktopApplicationLifetime => Current?.ApplicationLifetime as IClassicDesktopStyleApplicationLifetime;
     private static MainWindow? MainWindow => DesktopApplicationLifetime?.MainWindow as MainWindow;
+    private System.Timers.Timer _clickResetTimer;
+
+    private bool _openWindowAfterNextClick = false;
+    public void OpenMenu_Click(object? sender, EventArgs e)
+    {
+        if (MainWindow?.IsVisible == true)
+            return;
+
+        if (!_openWindowAfterNextClick)
+        {
+            _clickResetTimer.Start();
+            _openWindowAfterNextClick = true;
+        }
+        else
+        {
+            MainWindow?.ShowWindow();
+        }
+    }
 
     public void ShowWindow_Click(object? sender, EventArgs e)
     {
-        MainWindow?.RestoreWindowState();
+        MainWindow?.ToggleWindowVisibility();
     }
 
-    public void StartStopDevices_Click(object? sender, EventArgs e)
+    public void ToggleDevices_Click(object? sender, EventArgs e)
     {
-        //var isRunning = MainWindow?.DevicesRunning ?? false;
-        //var startStop = this
-        //if (isRunning)
-        //{
-    
-        //    StartStopMenuItem.Header == "Start Devices";
-        //} 
+        if (MainWindowViewModel is null)
+            return;
+
+        MainWindowViewModel.Run = !MainWindowViewModel.Run;
+        MainWindowViewModel?.ToggleRunning();
+
+        if (sender is NativeMenuItem item)
+        {
+            item.Header = (MainWindowViewModel!.Run ? "Stop" : "Start") + " devices";
+        }
     }
 
     public void RestartDevices_Click(object? sender, EventArgs e)
     {
-        MainWindow?.RestoreWindowState();
+        if(MainWindowViewModel?.Run != true) 
+            return;
+
+        MainWindowViewModel?.RestartDevices();
     }
 
     public void ExitApp_Click(object? sender, EventArgs e)
@@ -62,13 +106,68 @@ public class App : Application
     {
         if (DesktopApplicationLifetime is not null)
         {
-            DesktopApplicationLifetime.MainWindow = new MainWindow(Resolver.GetRequiredService<ThemeManager>())
+            MainWindowViewModel = Resolver.GetRequiredService<IMainWindowViewModel>();
+            var mainWindow = new MainWindow(Resolver.GetRequiredService<ThemeManager>())
             {
-                DataContext = Resolver.GetRequiredService<IMainWindowViewModel>(),
+                DataContext = MainWindowViewModel,
             };
+
+            DesktopApplicationLifetime.MainWindow = mainWindow;
+
+            if (MainWindowViewModel is INotifyPropertyChanged mwVmNpc)
+            {
+                mwVmNpc.PropertyChanged += MainWindowViewModel_PropertyChanged;
+            }
+
+            if (MainWindow is INotifyPropertyChanged mwNpc)
+            {
+                mwNpc.PropertyChanged += MainWindow_PropertyChanged;
+            }
+
+            _trayIcon = TrayIcon.GetIcons(this)[0];
+            UpdateTrayMenu();
         }
 
         base.OnFrameworkInitializationCompleted();
+    }
+
+    private void MainWindow_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(MainWindow.IsVisible))
+        {
+            UpdateTrayMenu();
+        }
+    }
+
+    private void MainWindowViewModel_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(IMainWindowViewModel.Run))
+        {
+            UpdateTrayMenu();
+        }
+    }
+
+    private void UpdateTrayMenu()
+    {
+        //// show window
+        //if (_trayIcon.Menu!.Items[0] is NativeMenuItem showWindow)
+        //{
+        //    showWindow.IsEnabled = MainWindow?.IsVisible != true;
+        //}
+
+        // Restart Devices
+        if (
+            _trayIcon.Menu!.Items[1] is NativeMenuItem devices &&
+            devices.Menu!.Items[1] is NativeMenuItem restartDevices)
+        {
+            restartDevices.IsEnabled = MainWindowViewModel?.Run == true;
+        }
+
+        // Reload plugins
+        if (_trayIcon.Menu!.Items[2] is NativeMenuItem reloadPlugins)
+        {
+            reloadPlugins.IsEnabled = MainWindowViewModel?.Run != true;
+        }
     }
 
     static IReadonlyDependencyResolver Resolver => Locator.Current;
