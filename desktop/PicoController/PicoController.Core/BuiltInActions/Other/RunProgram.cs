37beba5ae7addb3c80dfe2c01f26af11dabb234f
Epat <i.am@epat.xyz>
2023-02-08 09:30:28 +0100

    use cliwrap

--- desktop/PicoController/PicoController.Core/BuiltInActions/Other/RunProgram.cs
+++ desktop/PicoController/PicoController.Core/BuiltInActions/Other/RunProgram.cs
@@ -1,25 +1,29 @@
-﻿using PicoController.Core;
+﻿using CliWrap;
+using PicoController.Core;
 using System.Diagnostics;
 
 namespace PicoController.Core.BuildtInActions.Other;
 
 internal class RunProgram : IPluginAction
 {
-
     public async Task ExecuteAsync(int inputValue, string? argument)
     {
         if(argument is null)
             throw new ArgumentNullException("data");
 
         await Task.Yield();
-        var args = argument.Split(';');
-        var process = Process.Start(new ProcessStartInfo
+        var args = argument.Split(';', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+        var wrapper = Cli.Wrap(args[0]);
+
+        if (args.Length > 0)
         {
-            FileName = "CMD",
-            Arguments = "/C" + argument,
-            UseShellExecute = false,
-            CreateNoWindow = true,
-        });
+            for (int i = 1; i < args.Length; i++)
+            {
+                wrapper = wrapper.WithArguments(args[i]);
+            }
+        }
+
+        _ = wrapper.ExecuteAsync();
 
     }
 }
