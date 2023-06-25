2519ce19149654184c6faaf75ed46b3e25f6f96a
Epat <i.am@epat.xyz>
2023-06-26 01:01:00 +0200

    updating pico-sdk version and autogenerating inputs

--- Pico/controller/IO/Input/CreateInputs.h
+++ Pico/controller/IO/Input/CreateInputs.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "Input.h"
 #include "Button.h"
 #include "Encoder.h"
 #include "EncoderWithButton.h"
@@ -7,23 +8,5 @@
 
 namespace IO::Input
 {
-    static etl::vector<etl::unique_ptr<Input>, 256> CreateInputs()
-    {
-        etl::vector<etl::unique_ptr<Input>, 256> inputs;
-        uint8_t id = 0;
-        //Buttons
-        inputs.push_back(etl::unique_ptr<Input>(new Button(id++, 17)));
-        inputs.push_back(etl::unique_ptr<Input>(new Button(id++, 18)));
-        inputs.push_back(etl::unique_ptr<Input>(new Button(id++, 19)));
-        inputs.push_back(etl::unique_ptr<Input>(new Button(id++, 20)));
-
-        //Encoders with buttons
-        inputs.push_back(etl::unique_ptr<Input>(new EncoderWithButton(id++, 2, 3, 12)));
-        inputs.push_back(etl::unique_ptr<Input>(new EncoderWithButton(id++, 5, 4, 13)));
-        inputs.push_back(etl::unique_ptr<Input>(new EncoderWithButton(id++, 6, 7, 14)));
-        inputs.push_back(etl::unique_ptr<Input>(new EncoderWithButton(id++, 8, 9, 15)));
-        inputs.push_back(etl::unique_ptr<Input>(new EncoderWithButton(id++, 11, 10, 16)));
-
-        return inputs;
-    }
+    etl::vector<etl::unique_ptr<Input>, 256> CreateInputs();
 }
