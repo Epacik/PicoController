81f4a2137e2b71c91f1db25ba17d86cc0dc7f725
Epat <i.am@epat.xyz>
2023-09-20 02:26:12 +0200

    storage

---
+++ desktop/PicoController/PicoController.Gui/Plugin/Storage.cs
@@ -0,0 +1,45 @@
+﻿using PicoController.Plugin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PicoController.Gui.Plugin
+{
+    internal class Storage : IStorage
+    {
+        private readonly object _locker = new();
+        private readonly Dictionary<string, object?> _data = new();
+        public object? Get(string key)
+        {
+            lock (_locker)
+            {
+                if (_data.TryGetValue(key, out object? value))
+                    return value;
+            }
+
+            return null;
+        }
+
+        public T? GetAs<T>(string key)
+        {
+            var value = Get(key);
+
+            if (value is T tval)
+                return tval;
+
+            return default;
+        }
+
+        public void Set(string key, object? value)
+        {
+            lock (_locker) { _data[key] = value; }
+        }
+
+        public void SetAs<T>(string key, T? value)
+        {
+            lock (_locker) { _data[key] = value; }
+        }
+    }
+}
--- desktop/PicoController/PicoController.Plugin.Interfaces/IStorage.cs
+++ desktop/PicoController/PicoController.Plugin.Interfaces/IStorage.cs
@@ -4,9 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PicoController.Core
+namespace PicoController.Plugin
 {
-    public interface IStorageAccess
+    public interface IStorage
     {
+        public object? Get(string key);
+        public T? GetAs<T>(string key);
+        public void Set(string key, object? value);
+        public void SetAs<T>(string key, T? value);
     }
 }
